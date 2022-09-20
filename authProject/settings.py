a0fe4e34e7d06c4010df4bb7bfb16b0d2febe841
dquinterol <dpquinterol@unal.edu.co>
2022-09-20 09:48:42 -0500

    newsettings

--- authProject/settings.py
+++ authProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-jqrm9xnoi!l7qz=#y8t#stxd7c83_(!=*nn%sdp)05bp^75i29
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'authApp',
+    'corsheaders',
 ]
 
 SIMPLE_JWT={
@@ -61,6 +62,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS=[
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
 ]
 
 REST_FRAMEWORK={
@@ -139,6 +148,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+
 
 
 # Static files (CSS, JavaScript, Images)
